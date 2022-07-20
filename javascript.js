240bd31b6205dbd7c2247d454dc9041bd517b687
Akshay <ahanooman0307@gmail.com>
2022-07-20 02:35:04 -0400

    deleted useless functions and made working algorithm

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+
+
+let playerChoice = "";
+
+
+
 function getComputerChoice(){ //randomly selects a choice for the computer
     let computerChoice = ["Rock", "Paper", "Scissors"];
     let random = computerChoice[Math.floor(Math.random()*3)]; //chooses random index from 0-2 of computerChoice
@@ -5,78 +11,76 @@ function getComputerChoice(){ //randomly selects a choice for the computer
     return random;
 }
 
-function getPlayerChoice(){ //prompts use to choose rock paper or scissors
-    let playerChoice = capitalize(prompt("Choose Rock, Paper, or Scissors"));
-    if(playerChoice == "Rock"){
-        check = 1;
-    }
-    else if(playerChoice == "Paper"){
-        check = 1;
-    }
-    else if(playerChoice == "Scissors"){
-        check = 1;
-    }
-    //makes sures players types a valid answer
-    while(check == 0)
-    {   
-        playerChoice = capitalize(prompt("Please choose Rock, Paper, or Scissors"));
-        if(playerChoice == "Rock"){
-            check = 1;
-        }
-        else if(playerChoice == "Paper"){
-            check = 1;
-        }
-        else if(playerChoice == "Scissors"){
-            check = 1;
-        }
-    }
-    check = 0;
-    return playerChoice;
-}
-
-function capitalize(word){
-    let result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    return result; 
-}
 
 function playRound(computerChoice, playerChoice){
     if(playerChoice == computerChoice)
     {
+        console.log("a");
         return "It's a draw!";
     }
     else if((playerChoice == "Rock" && computerChoice == "Paper") || (playerChoice == "Paper" && computerChoice == "Scissors"))
     {   
         lose++;
+        console.log("b");
         return "You lose the computer chose " + computerChoice + "!";
     }
     else if((playerChoice == "Rock" && computerChoice == "Scissors") ||(playerChoice == "Scissors" && computerChoice == "Paper"))
     {   
         win++;
+        console.log("c");
         return "You win the computer chose " + computerChoice + "!";
+        
     }
 }
+
 
 function game(){
 
     for(let i = 0; i < 5; i++){
         let computerChoice = getComputerChoice();
-        let playerChoice = capitalize(getPlayerChoice());
+        let playerchoice = playerChoice;
         playRound(computerChoice, playerChoice);
-        console.log(playRound(computerChoice, playerChoice));
+        alert(playRound(computerChoice, playerChoice));
 
     }
     if(win > lose){
-        console.log("You win!");
+        alert("You win!");
     }
     else{
-        console.log("You lose!");
+        alert("You lose!");
     }
   
 }
 
+// game();
+
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener('click', () => {
+    console.log("Rock");
+    let computerChoice = getComputerChoice();
+    playerChoice = "Rock";
+    playRound(computerChoice, playerChoice);
+
+})
+paper.addEventListener('click', () =>{
+    console.log("Paper");
+    let computerChoice = getComputerChoice();
+    playerChoice = "Paper";
+    playRound(computerChoice, playerChoice);
+})
+scissors.addEventListener('click', () => {
+    console.log("Scissors");
+    let computerChoice = getComputerChoice();
+    playerChoice = "Scissors";
+    playRound(computerChoice, playerChoice);
+})
+
+
+
 let win = 0;
 let lose = 0;
 let check = 0;
-game();
-
-
